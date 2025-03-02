8ded61e241b5174df71f92e8f09ba3da8faadfb7
carlmacabales <carlmacabales31@gmail.com>
2025-03-02 13:57:54 +0800

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+import onboarding1 from "@/assets/images/onboarding1.png";
+import onboarding2 from "@/assets/images/onboarding2.png";
+import onboarding3 from "@/assets/images/onboarding3.png";
+
+export const onboarding = [
+    {
+        id: 1,
+        title: 'Welcome to PetChinGu!',
+        description: "Your pet's well-being, simplified!",
+        image: onboarding1,
+    },
+    {
+        id: 2,
+        title: 'Track. Manage. Care.',
+        description: "Stay organized with pet needs!",
+        image: onboarding2,
+    },
+    {
+        id: 3,
+        title: "Let's Get Started!",
+        description: "Set up your pet's profile",
+        image: onboarding3,
+    },
+]
---
+++ lib/data.ts
@@ -0,0 +1,46 @@
+export const galleryImages = [
+  "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+  "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/29708160-9b39-42d0-a5ed-4f8b9c85a267/labrador+retriever+dans+pet+care.jpeg",
+  "https://wallpapercave.com/wp/wp4385357.jpg",
+  "https://hips.hearstapps.com/hmg-prod/images/small-dogs-pug-1563779089.jpg?crop=0.668xw:1.00xh;0.311xw,0&resize=980:*",
+  "https://www.yourtango.com/sites/default/files/2020/golden-retriever.jpg",
+  "https://i.redd.it/v82wta66q5971.jpg",
+  "https://d2zp5xs5cp8zlg.cloudfront.net/image-83814-800.jpg",
+  "https://www.womansworld.com/wp-content/uploads/2024/08/cute-cats.jpg?quality=86&strip=all",
+  "https://i.pinimg.com/550x/07/89/3f/07893fab8e043255148a3afeba55687a.jpg",
+  "https://www.peta.org/wp-content/uploads/2021/04/rabbit-blue-background.jpg",
+];
+
+export const agentImages = [
+  "https://images.unsplash.com/photo-1691335053879-02096d6ee2ca?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1544723495-432537d12f6c?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1542507464418-09c375b86bbe?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+export const reviewImages = [
+  "https://images.unsplash.com/photo-1517331671191-ddc2c6d3ebd1?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1474176857210-7287d38d27c6?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1511551203524-9a24350a5771?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=60&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+export const propertiesImages = [
+  "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+  "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/29708160-9b39-42d0-a5ed-4f8b9c85a267/labrador+retriever+dans+pet+care.jpeg",
+  "https://wallpapercave.com/wp/wp4385357.jpg",
+  "https://hips.hearstapps.com/hmg-prod/images/small-dogs-pug-1563779089.jpg?crop=0.668xw:1.00xh;0.311xw,0&resize=980:*",
+  "https://www.yourtango.com/sites/default/files/2020/golden-retriever.jpg",
+  "https://i.redd.it/v82wta66q5971.jpg",
+  "https://d2zp5xs5cp8zlg.cloudfront.net/image-83814-800.jpg",
+  "https://www.womansworld.com/wp-content/uploads/2024/08/cute-cats.jpg?quality=86&strip=all",
+  "https://i.pinimg.com/550x/07/89/3f/07893fab8e043255148a3afeba55687a.jpg",
+  "https://www.peta.org/wp-content/uploads/2021/04/rabbit-blue-background.jpg",
+  "https://w0.peakpx.com/wallpaper/209/34/HD-wallpaper-cute-puppy-dog-dogs-puppies-sweet-white-thumbnail.jpg",
+  "https://static.boredpanda.com/blog/wp-content/uploads/2018/08/5b7d549bbc16d_Phjc_3__qy9XK2ZXD70WocYN77vZBrHn0VdspylPT0M__700.jpg",
+  "https://www.usatoday.com/gcdn/media/2020/02/25/USATODAY/usatsports/MotleyFool-TMOT-0107af3a-pet-dog-food.jpg?width=700&height=467&fit=crop&format=pjpg&auto=webp",
+  "https://i.guim.co.uk/img/media/43352be36da0eb156e8551d775a57fadba8ae6d7/0_0_1440_864/master/1440.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1829611852af3ffc6460b4068e20bcbc",
+  "https://hips.hearstapps.com/hmg-prod/images/cutest-cat-breeds-persian-cat-663a8c3f02bbe.jpg?crop=0.668xw:1.00xh;0.139xw,0&resize=980:*",
+];
